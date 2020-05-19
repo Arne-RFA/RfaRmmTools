c7ca4a2113b3d372f40ceef0cf4323ceb32a3ba3
Tony Pagliaro <tony@pagliaro.co>
2020-05-19 18:30:28 -0400

    Create RfaRmmTools.psm1

---
+++ RfaRmmTools.psm1
@@ -0,0 +1,29 @@
+# Create a web session
+$web = New-Object Net.WebClient
+
+
+# Load other modules
+$OtherModules = @(
+    'https://raw.githubusercontent.com/RFAInc/RfaCwaDeploy/master/RfaCwaDeploy.psm1'
+)
+Foreach ($uri in $OtherModules) {
+    $web.DownloadString($uri) | Invoke-Expression
+}
+
+
+# Create generic aliases for vendor-specific functions
+New-Alias -Name 'Install-RfaRmmAgent' -Value 'Install-RfaCwaAgent' # Parameter LocationID must be at Position=0
+
+
+# Load external functions (when you don't need the entire module for a few tasks)
+$ExternalFunctions = @(
+    'https://raw.githubusercontent.com/tonypags/PsWinAdmin/master/Get-CimLocalDisk.ps1'
+)
+Foreach ($uri in $ExternalFunctions) {
+    $web.DownloadString($uri) | Invoke-Expression
+}
+
+
+# Clean up
+$web.Dispose | Out-Null
+
